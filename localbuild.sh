58adff9760788e4964966bfcd001ea843822606a
Elek, Márton <elek@users.noreply.github.com>
2016-12-15 16:40:24 +0100

    master and slave server for spark

--- localbuild.sh
+++ localbuild.sh
@@ -1,2 +1,4 @@
 docker build -t elek/spark-base spark-base
 docker build -t elek/spark-history spark-history
+docker build -t elek/spark-master spark-master
+docker build -t elek/spark-slave spark-slave
---
+++ spark-slave/starter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_HOST
