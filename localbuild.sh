8760cc653bc43b779ca2d0a3829f48f8ea06957d
Elek, Márton <elek@users.noreply.github.com>
2017-02-26 22:23:21 +0100

    sync to the master

--- localbuild.sh
+++ localbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_TAG=${DOCKER_TAG:-2.1.0}
+DOCKER_TAG=${DOCKER_TAG:-latest}
 docker build -t elek/spark-base:$DOCKER_TAG spark-base
 docker build -t elek/spark-history:$DOCKER_TAG spark-history
 docker build -t elek/spark-master:$DOCKER_TAG spark-master
