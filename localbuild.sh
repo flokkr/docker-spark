fc3fd6758e29c584a353d49bb3d3dbc2b75e2c10
Elek, Márton <elek@users.noreply.github.com>
2017-07-21 14:18:10 +0200

    remove common scripts

--- localbuild.sh
+++ localbuild.sh
@@ -1,7 +1,8 @@
 set -e
 DOCKER_TAG=${1:-latest}
-docker build -t elek/hadoop:$DOCKER_TAG hadoop
-docker build -t elek/hadoop-hdfs-datanode:$DOCKER_TAG hdfs-datanode
-docker build -t elek/hadoop-hdfs-namenode:$DOCKER_TAG hdfs-namenode
-docker build -t elek/hadoop-yarn-nodemanager:$DOCKER_TAG yarn-nodemanager
-docker build -t elek/hadoop-yarn-resourcemanager:$DOCKER_TAG yarn-resourcemanager
+BASE_IMAGE=$(basename $(dirname $(find -name baseimage)))
+#docker build -t elek/$BASE_IMAGE:$DOCKER_TAG $BASE_IMAGE
+find -name Dockerfile | grep -v "/$BASE_IMAGE/" | while read -r line; do
+   NAME=$(basename $(dirname $line))
+   docker build -t elek/$NAME:$DOCKER_TAG $NAME/
+done
