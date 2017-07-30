34789db919da4256b345c5212f8dbf2cb5173eef
Elek, Márton <elek@users.noreply.github.com>
2017-07-30 14:34:44 +0200

    switch to the travis based flokkr build with the latest base image

--- localbuild.sh
+++
@@ -1,8 +0,0 @@
-set -e
-DOCKER_TAG=${1:-latest}
-BASE_IMAGE=$(basename $(dirname $(find -name baseimage)))
-#docker build -t elek/$BASE_IMAGE:$DOCKER_TAG $BASE_IMAGE
-find -name Dockerfile | grep -v "/$BASE_IMAGE/" | while read -r line; do
-   NAME=$(basename $(dirname $line))
-   docker build -t elek/$NAME:$DOCKER_TAG $NAME/
-done
